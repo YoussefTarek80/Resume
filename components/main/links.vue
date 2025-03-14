45ecb6538814876455e86a35b353c28c2cfaa7e4
Youssef Tarek <youssef.tarek1090@gmail.com>
2025-03-14 17:37:43 +0200

    Updated_v3

--- components/main/links.vue
+++ components/main/links.vue
@@ -1,23 +1,35 @@
 <template>
     <header class="flex items-end justify-end">
         <nav class="bg-[#2E2E2E] rounded-r-3xl rounded-l-sm rounded-bl-3xl rounded-br-sm p-3">
-            <ul class="flex items-center ">
-                <nuxt-link to="/main/about">
+            <ul class="flex items-center">
+                <nuxt-link 
+                    to="/main/about" 
+                    :class="{'text-[#cca636]': route.path === '/main/about'}"
+                >
                     <li class="px-6 py-3 hover:text-[#cca636] cursor-pointer">
                         About
                     </li>
                 </nuxt-link>
-                <nuxt-link to="/main/resume">
+                <nuxt-link 
+                    to="/main/resume" 
+                    :class="{'text-[#cca636]': route.path === '/main/resume'}"
+                >
                     <li class="px-6 py-3 hover:text-[#cca636] cursor-pointer">
                         Resume
                     </li>
                 </nuxt-link>
-                <nuxt-link to="/main/portfolio">
+                <nuxt-link 
+                    to="/main/portfolio" 
+                    :class="{'text-[#cca636]': route.path === '/main/portfolio'}"
+                >
                     <li class="px-6 py-3 hover:text-[#cca636] cursor-pointer">
                         Portfolio
                     </li>
                 </nuxt-link>
-                <nuxt-link to="/main/contact">
+                <nuxt-link 
+                    to="/main/contact" 
+                    :class="{'text-[#cca636]': route.path === '/main/contact'}"
+                >
                     <li class="px-6 py-3 hover:text-[#cca636] cursor-pointer">
                         Contact
                     </li>
@@ -25,5 +37,10 @@
             </ul>
         </nav> 
     </header>
-
 </template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="grid lg:grid-cols-4 md:grid-cols-2 lg:gap-32 md:gap-0 sm:gap-0 sm:grid-cols-1">
-        <div class="sm:col-span-1 col-span-3  sm:p-20 p-10 ">
+    <div class="grid lg:grid-cols-4 md:grid-cols-3 lg:gap-32 md:gap-0 sm:gap-0 sm:grid-cols-1">
+        <div class=" lg:col-span-1 sm:col-span-1  md:col-span-1  sm:p-20 p-10 ">
             <sideBar />
         </div>
-        <div class="col-span-3  sm:p-20 p-10">
+        <div class="lg:col-span-3 md:col-span-2 sm:col-span-1  sm:p-20 p-10">
             <NuxtLayout name="main">
                 <slot />
             </NuxtLayout>
         </div>
     </div>
+
 </template>
 <script setup> 
     useHead({
@@ -21,13 +22,24 @@
             href:'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sigmar&display=swap'
         },
         { rel: 'icon', type: 'image/png', href: '/YT.png' },
-        { rel: 'icon', type: 'image/x-icon', href: '/YT.png' },
-    ],
+        { rel: 'icon', type: 'image/png', href: '/YT.png' },],
         
     })
 
 </script>
 <style>
+@media (max-width: 1272px) {
+    .grid {
+        display: flex;
+        flex-direction: column;
+    }
+}
+@media (max-width: 551px) {
+    .grid {
+        display: grid;
+        grid-template-columns: 1fr; 
+    }
+}
 body{
     font-family: "Poppins", sans-serif;
     font-weight: 400;
